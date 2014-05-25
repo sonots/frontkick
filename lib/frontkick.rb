232fd22e21423d811d579bad498d31622bb73b87
sonots <sonots@gmail.com>
2014-05-25 20:28:16 +0900

    fix Frontkick.exec

--- lib/frontkick.rb
+++ lib/frontkick.rb
@@ -3,8 +3,7 @@ require 'frontkick/command'
 require 'frontkick/command_result'
 
 module Frontkick
-  def exec(cmd, opts = {})
+  def self.exec(cmd, opts = {})
     ::Frontkick::Command.exec(cmd, opts)
   end
-  module_function :exec
 end
