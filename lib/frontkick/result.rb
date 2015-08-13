f4e9d32cd641fa19bcf18f1f30ba0042af7d2ba1
Naotoshi Seo <sonots@gmail.com>
2015-08-13 23:20:15 +0900

    Compatibility with with Open3 Process::Status

--- lib/frontkick/result.rb
+++ lib/frontkick/result.rb
@@ -3,6 +3,8 @@ module Frontkick
     attr_accessor :stdout, :stderr, :exit_code, :duration
     alias :status :exit_code
     alias :status= :exit_code=
+    alias :exitstatus :exit_code # for compatibility with Open3 Process::Status
+    alias :exitstatus= :exit_code= # for compatibility with Open3 Process::Status
 
     def initialize(params)
       @stdout = params[:stdout] || ""
@@ -22,6 +24,7 @@ module Frontkick
     def successful?
       @exit_code == 0
     end
+    alias :success? :successful? # for compatibility with Open3 Process::Status
   end
   CommandResult = Result # for lower version compatibility
 end
